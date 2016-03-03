7788762a140d67e67f00089fb7c1ec478ba89341
Sorokanyuk IR <Sorokanyuk@gmail.com>
2016-03-03 18:37:45 +0300

    c# Chat bot
    Only en

---
+++ c# ChatBot/ChatBot/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AIMLbot;
+
+namespace ChatBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot myBot = new Bot();
+            myBot.loadSettings();
+            User myUser = new User("consoleUser", myBot);
+            myBot.isAcceptingUserInput = false;
+            myBot.loadAIMLFromFiles();
+            myBot.isAcceptingUserInput = true;
+            Console.Write("Hello. English please \n");
+            while (true)
+            {
+                Console.Write("Вы: ");
+                string input = Console.ReadLine();
+                if (input.ToLower() == "выход")
+                {
+                    break;
+                }
+                else
+                {
+                    Request r = new Request(input, myUser, myBot);
+                    Result res = myBot.Chat(r);
+                    Console.WriteLine("Бот: " + res.Output);
+                }
+            }
+        }
+    }
+}
