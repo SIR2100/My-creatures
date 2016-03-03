e5867471c624ebb1cd9cbd1b5c6e9d2b8115c690
Sorokanyuk IR <Sorokanyuk@gmail.com>
2016-03-03 18:39:47 +0300

    c# from 100 to 2 with +2 *3
    Алгоритм, нужно найти кратчайшее решение от 100 до 2 используя +2 *3

---
+++ c# FindEasy/FindEasy/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindEasy
+{
+    class Program
+    {
+        static int main()
+        {
+            int start = 2;
+            int to = 100;
+            Queue<State> states = new Queue<State>();
+            states.Enqueue(new State(start, null, ""));
+            State now = states.Dequeue();
+            while (now.Value != to)
+            {
+                states.Enqueue(new State(now.Value + 3, now, "+3"));
+                states.Enqueue(new State(now.Value * 2, now, "*2"));
+                now = states.Dequeue();
+            }
+            string result = "";
+            int count = 1;
+            while (now.Parent != null)
+            {
+                result = now.Name + result;
+                now = now.Parent;
+                count++;
+            }
+            return count;
+        }
+
+        static string task_2_2(int value, int to, string result, int count)
+        {
+            if (value == to)
+            {
+                return result;
+            }
+            if (count == 0)
+            {
+                return "";
+            }
+            List<string> res = new List<string>();
+            res.Add(task_2_2(value + 3, to, result + "+3", count - 1));
+            res.Add(task_2_2(value * 2, to, result + "*2", count - 1));
+            string min = "";
+            foreach (var item in res)
+            {
+                if (item.Length > 0 && item.Length < min.Length && min.Length > 0)
+                {
+                    min = item;
+                }
+                else if (min.Length == 0)
+                {
+                    min = item;
+                }
+            }
+
+            return min;
+        }
+
+        static void Main(string[] args)
+        {
+            int x = main();
+            for (int i = 0; i < 100; i++)
+            {
+                string result = task_2_2(2, 100, "", i);
+                if (result == "")
+                {
+                    continue;
+                }
+                Console.WriteLine("2 (" + result + ") =100");
+                Console.WriteLine("Шагов: " + i);
+                break;
+            }
+            
+            Console.Read();
+
+        }
+    }
+}
---
+++ c# FindEasy/FindEasy/State.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindEasy
+{
+    class State
+    {
+        public State Parent;
+        public int Value;
+        public string Name;
+        public State(int v, State p, string n)
+        {
+            Parent = p;
+            Value = v;
+            Name = n;
+        }
+    }
+}
